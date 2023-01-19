e60d20df0d3b2ea95c26047f9f929acf90a3c7c4
KoRoTa-school@outlook.jp <KoRoTa-school@outlook.jp>
2023-01-19 11:04:41 +0900

    playerの体のモデル追加

--- SourceFiles/scene/play/Player.h
+++ SourceFiles/scene/play/Player.h
@@ -8,6 +8,9 @@ class Player : public BoxCollider
 {
 private:
 	Model* model_ = nullptr;
+	Model* modelBody_ = nullptr;	//体のモデル
+	Model* modelLegL_ = nullptr;	//左足のモデル
+	Model* modelLegR_ = nullptr;	//右足のモデル
 	float spdX = -0.1f;
 	float epsilon = 1.0e-5f; // 当たり判定のときの微調整として使う
 	Jump jump;
