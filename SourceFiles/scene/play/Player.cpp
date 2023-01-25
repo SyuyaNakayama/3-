5e2371f07302ab004ba93088a7a947408d282a03
KoRoTa-school@outlook.jp <KoRoTa-school@outlook.jp>
2023-01-25 14:55:59 +0900

    昇るモーション改善

--- SourceFiles/scene/play/Player.cpp
+++ SourceFiles/scene/play/Player.cpp
@@ -109,9 +109,16 @@ void Player::Update()
 		break;
 	}
 
-	WalkMotion();
+	//プレイヤーが向いている方向を求める
+	if (oldPos > worldTransform.translation_.x) { direction = 0; }
+	else if (oldPos < worldTransform.translation_.x) { direction = 1; }
 
+	WalkMotion();
 	worldTransform.Update();
+
+	//現在の座標を保存する
+	oldPos = worldTransform.translation_.x;
+
 	isClimb = isLadderHit = false;
 
 	parentWorldTransform_[PartId::kBody].Update();
@@ -163,13 +170,13 @@ void Player::OnCollision(BoxCollider* boxCollider)
 
 	// それ以外なら撥ね返る
 	spdX = -spdX;
-
-	if (direction == 0) { direction = 1; }
-	else if (direction == 1) { direction = 0; }
 }
 
 void Player::OnCollision(IncludeCollider* includeCollider)
 {
 	isClimb = true;
-	if (isLadderHit) { worldTransform.translation_.y += 0.01f; }
+	if (isLadderHit) {
+		worldTransform.translation_.y += 0.01f;
+		direction = 2;
+	}
 }
--- SourceFiles/scene/play/Player.h
+++ SourceFiles/scene/play/Player.h
@@ -20,10 +20,10 @@ private:
 	WorldTransform parentWorldTransform_[3] = {}; //親子関係用のworldTransform
 
 	uint16_t direction = 0;	//キャラの方向(0...左, 1...右, 2...奥)
-	float walkTimer = 0;
 	bool walkFlag = true;
 	float walkPos = 0.0f;
 	bool ForB = true; //true...前, false...後
+	float oldPos = 0.0f;
 
 	Input* input = nullptr;
 
